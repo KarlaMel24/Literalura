df7c7757506fd29c9b7f98d072d4fd8b31bda165
Karla <karlamel248@gmail.com>
2025-01-15 18:06:20 -0600

    Structure of the project

---
+++ src/main/java/com/Literalura/AluraChallenge2/AluraChallenge2Application.java
@@ -0,0 +1,13 @@
+package com.Literalura.AluraChallenge2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluraChallenge2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AluraChallenge2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/Literalura/AluraChallenge2/AluraChallenge2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Literalura.AluraChallenge2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AluraChallenge2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
